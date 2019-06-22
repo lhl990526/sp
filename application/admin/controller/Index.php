a0e7e5790ed2083437518bd01e67581a4a56611f
刘泓李 <522898206@qq.com>
2019-06-22 13:59:28 +0800

    linux

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,16 +1,19 @@
 <?php
 namespace app\admin\controller;
+
 use gmars\rbac\Rbac;
+
 class Index extends Common
 {
     public function index()
     {
        return $this->fetch();
     }
-
-    public function rbac(){
+     public function rbac(){
     	$rbac = new Rbac();
     	$rbac->createTable();
     }
+
+
 
 }
